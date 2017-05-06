6639b11d11e52c582b7a629d8122f30ab33a3ef3
Christopher <christophermarkbell@gmail.com>
2017-05-06 14:03:53 -0700

    bug fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	crystals = ["assets/images/red.png","assets/images/blue.png", "assest/images/yellow.png", "assets/images/green.png"];
+	crystals = ["assets/images/red.png","assets/images/blue.png", "assets/images/yellow.png", "assets/images/green.png"];
 
 	var counter = 0;
 	var wins = 0;
@@ -29,7 +29,7 @@ $(document).ready(function() {
 				for (var i = 0; i < numbers.length; i++) {
 					var imageCrystal = $("<img></img>");
 					imageCrystal.attr("data-num", (numbers[i]));
-					imageCrystal.attr("src" (crystals[i]));
+					imageCrystal.attr("src",(crystals[i]));
 					imageCrystal.attr("alt", "crystals");
 					imageCrystal.addClass("crystalImage");
 					$("#crystals").append(imageCrystal);
